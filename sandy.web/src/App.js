1ba5141f20fc2b3f1edc33b372a14c85a1133862
SandyHoffmann <manglesan98@gmail.com>
2024-01-31 23:39:57 -0300

    New visual settings

--- sandy.web/src/App.js
+++ sandy.web/src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import { Icon } from '@iconify/react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='App-header'>
+        <div className='App-header-icons'>
+          <div><Icon icon="nimbus:whatsapp" width="24" height="24"/></div></div>
+        <div className='App-header-title'><p><b>Sandy Hoffmann</b></p></div>
+        <div className='App-header-icons'>
+          <div>
+        <Icon icon="nimbus:exclamation-triangle" width="24" height="24"/>
+        </div>
+        <div>
+        <Icon icon="nimbus:close" width="24" height="24"/>
+        </div>
+        </div>
       </header>
+      <div className='App-content'>
+        <div className='content-header'>
+          <div className='content-header-pages'>
+            <div>
+              <Icon icon="nimbus:home" width="24" height="24"/>
+            </div>
+            <div>
+              <p>About Me</p>
+            </div>
+            <div>
+              <p>My Projects</p>
+            </div>
+            <div>
+              <p>Certificates</p>
+            </div>
+            <div>
+              <p>Freelance - Contact Me</p>
+            </div>
+          </div>
+          <div className='content-header-icons'>
+            <div>
+              <Icon icon="nimbus:search" width="24" height="24"/>
+            </div>
+            <div>
+              <Icon icon="nimbus:question-circle" width="24" height="24"/>
+            </div>
+          </div>
+        </div>
+        <div className='content-body rainbow-bg'>
+          <h1>Montando página</h1>
+        </div>
+      </div>
     </div>
   );
 }
